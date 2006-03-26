3fa5eecdcccd5df285701540585de7d526dcf1a6
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-03-26 17:01:40 +0000

    get() now takes an explicit file name extension argument, rather than assuming "x10".

--- src/org/eclipse/imp/smapi/SmapBuilder.java
+++ src/org/eclipse/imp/smapi/SmapBuilder.java
@@ -4,36 +4,42 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class SmapBuilder {
-	public static String get(String filename, String pathPrefix, ArrayList /*LineElem*/ elems){
-		String name = filename;
-		boolean path = false;
-		if (filename.indexOf("/") != -1) { //path name included
-			int i = filename.lastIndexOf("/");
-			name = filename.substring(i+1);
-			filename = filename.substring(pathPrefix.length());
-			System.out.println("filename = " + filename);
-			path = true;
-		}
-		
-		
-		String info = "SMAP\n";
-	    info += name + ".java\n";
-	    info += "X10\n";
-	    info += "*S X10\n";
-	    info += "*F\n";
-	    if (path)
-	    	info += "+ ";
-	    info += "1 " + name + ".x10\n";
-	    if (path)
-	    	info += filename + ".x10" + "\n";
-	    info += "*L\n";
-	    
-	    for(Iterator t = elems.iterator(); t.hasNext(); ){
-	    	LineElem elem = (LineElem) t.next();
-	    	info += elem.getX10() + "#1:" + elem.getStart() +"," + elem.getIncr() +"\n";
-	    }
-	    
-	    info += "*E\n";
-	    return info;
-	}
+    /**
+     * @param filename
+     * @param pathPrefix
+     * @param elems
+     * @param fileExten file name extension of the original source file; should not have a leading "."
+     * @return the SMAP attribute contents, as specified by JSR 045
+     */
+    public static String get(String filename, String pathPrefix, ArrayList/*<LineElem>*/ elems, String fileExten) {
+        String name= filename;
+        boolean path= false;
+        if (filename.indexOf("/") != -1) { // path name included
+            int i= filename.lastIndexOf("/");
+            name= filename.substring(i + 1);
+            filename= filename.substring(pathPrefix.length());
+            System.out.println("filename = " + filename);
+            path= true;
+        }
+
+        String info= "SMAP\n";
+        info+= name + ".java\n";
+        info+= fileExten + "\n";
+        info+= "*S " + fileExten + "\n";
+        info+= "*F\n";
+        if (path)
+            info+= "+ ";
+        info+= "1 " + name + "." + fileExten + "\n";
+        if (path)
+            info+= filename + "." + fileExten + "\n";
+        info+= "*L\n";
+
+        for(Iterator t= elems.iterator(); t.hasNext();) {
+            LineElem elem= (LineElem) t.next();
+            info+= elem.getOrigSrcStart() + "#1:" + elem.getJavaStart() + "," + elem.getIncr() + "\n";
+        }
+
+        info+= "*E\n";
+        return info;
+    }
 }
