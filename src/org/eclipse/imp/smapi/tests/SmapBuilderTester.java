3d1d596771474fc96b0ed858e3125188c72eecd6
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-03-26 17:02:50 +0000

    Changed test data to agree with what new generic (non-x10-specific) SmapBuilder produces (basically, just the case of the "section name").

--- src/org/eclipse/imp/smapi/tests/SmapBuilderTester.java
+++ src/org/eclipse/imp/smapi/tests/SmapBuilderTester.java
@@ -8,26 +8,26 @@ import com.ibm.watson.smapi.SmapBuilder;
 import junit.framework.TestCase;
 
 public class SmapBuilderTester extends TestCase {
-	public void test1(){
-		LineElem l1 = new LineElem(1,2,3);
-		LineElem l2 = new LineElem(4,5,6);
-		ArrayList s = new ArrayList(); 
-		s.add(l1);
-		s.add(l2);
-		String smap = SmapBuilder.get("foo", null, s);
-		String info = "SMAP\n";
-	    info += "foo.java\n";
-	    info += "X10\n";
-	    info += "*S X10\n";
-	    info += "*F\n";
-	    info += "1 foo.x10\n";
-	    info += "*L\n";
-	    info += "1#1:2,3\n";
-	    info += "4#1:5,6\n";
-	    info += "*E\n";
-	    if (! info.equals(smap)){
-	    	System.err.println("test failed");
-	    	System.out.println(smap);
-	    }
-	}
+    public void test1() {
+        LineElem l1= new LineElem(1, 2, 3);
+        LineElem l2= new LineElem(4, 5, 6);
+        ArrayList s= new ArrayList();
+        s.add(l1);
+        s.add(l2);
+        String smap= SmapBuilder.get("foo", null, s, "x10");
+        String info= "SMAP\n";
+        info+= "foo.java\n";
+        info+= "x10\n";
+        info+= "*S x10\n";
+        info+= "*F\n";
+        info+= "1 foo.x10\n";
+        info+= "*L\n";
+        info+= "1#1:2,3\n";
+        info+= "4#1:5,6\n";
+        info+= "*E\n";
+        if (!info.equals(smap)) {
+            System.err.println("test failed");
+            System.out.println(smap);
+        }
+    }
 }
